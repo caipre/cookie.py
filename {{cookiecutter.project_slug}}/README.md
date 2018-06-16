2cb91fbb2c1dd16e40235e8164b21e162583013f
Nick Platt <platt.nicholas@gmail.com>
2018-06-16 12:52:04 -0400

    initial commit

---
+++ {{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,5 @@
+{{cookiecutter.project_name}}
+======
+
+![PyPi](https://img.shields.io/pypi/v/{{cookiecutter.project_slug}}.svg)
+![Travis](https://img.shields.io/travis/{{cookiecutter.github_username}}/{{cookiecutter.project_slug}}.svg)
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,15 @@
+__name__ = '{{ cookiecutter.project_slug }}'
+__version__ = '0.0.0'
+__license__ = 'Apache-2.0'
+__author__ = '{{ cookiecutter.author_name }}'
+__email__ = '{{ cookiecutter.author_email }}'
+__url__ = '{{ cookiecutter.project_url }}'
+__description__ = '''
+{{ cookiecutter.project_description }}
+'''
+__keywords__ = ['{{ cookiecutter.project_keywords }}']
+__classifiers__ = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+]
